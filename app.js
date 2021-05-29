36f131d84ca6e71688f37fab3c97dfa60199b5ca
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2021-05-29 10:52:32 +0300

    init: start learning

---
+++ app.js
@@ -0,0 +1,18 @@
+//переменные
+
+var name = 'Dima' // string
+const lastName = 'Kuzminykh' // string
+let age = 33 // number
+age = 30;
+name = 'Dim'
+
+console.log(name)
+console.log(lastName)
+console.log(age)
+
+//const нельзя изменить
+//let можно изменить
+//var не использовать.
+//стараться использовать только const
+
+const isProgrammer = true // boolean
