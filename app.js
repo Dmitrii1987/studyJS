2a5f43f7d97605f5d4569d18e6b0158f76cf7ff2
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2021-06-03 18:07:42 +0300

    feat: update js file

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-//переменные
+// 1 ПЕРЕМЕННЫЕ
 
-var name = 'Dima' // string
-const lastName = 'Kuzminykh' // string
-let age = 33 // number
-age = 30;
-name = 'Dim'
-
-console.log(name)
-console.log(lastName)
-console.log(age)
+//var name = 'Dima' // string
+//const lastName = 'Kuzminykh' // string
+//const age = 33 // number
+//const isProgrammer = true // boolean
 
 //const нельзя изменить
 //let можно изменить
 //var не использовать.
 //стараться использовать только const
 
-const isProgrammer = true // boolean
+
+// 2 МУТИРОВАНИЕ
+
+//console.log('Имя человека: ' + name  + ', а возраст: ' + age)
+//alert('Имя человека: ' + name  + ', а возраст: ' + age)
+//const surname = prompt('Введите фамилию')
+//alert(name + ' ' + surname)
+
+// 3 ОПЕРАТОРЫ
+
+//let currentYear = 2021
+//const birthYear = 1987
+//const age = currentYear - birthYear
+
+//const a = 10
+//const b = 5
+//let c = 32
+
+//c += a      // c = c + a
+//c -= a      // c = c - a
+//c *= a      // c = c * a
+//c /= a      // c = c / a
+
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+//console.log(++currentYear)
+//console.log(currentYear++)
+//console.log(--currentYear)
+//console.log(currentYear--)
+//console.log(c)
+
+// 4 ТИПЫ ДАННЫХ
+
+const isProgrammer = true
+const name = 'Dima'
+const age = 33
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
